ee91d9eb5076f9787f04f749fec997259dc67734
Rafael <rafael.avaria@gmail.com>
2022-11-09 12:53:17 -0300

    System Ready

--- chat_system/includes/db_connect.php
+++ chat_system/includes/db_connect.php
@@ -1,10 +1,11 @@
 <?php
 error_reporting(0);
-$conn = mysqli_connect("127.0.0.1","root","my_secret_pw_shh","chat_db_new");
+$conn = mysqli_connect(getenv("DB_HOST"),getenv("DB_USER"),getenv("DB_PASS"),getenv("DB_NAME"));
 session_start();
 // Check connection
 if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+{
+      echo "Failed to connect to MySQL: " . mysqli_connect_error() ;
+}
+
 ?> 
