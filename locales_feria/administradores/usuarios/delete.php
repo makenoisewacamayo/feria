30b17f6309ced00d119c1b88deb2d6a99043d75e
Rafael <rafael.avaria@gmail.com>
2022-11-11 12:40:14 -0300

    uso docker compose

--- locales_feria/administradores/usuarios/delete.php
+++
@@ -1,9 +0,0 @@
-<?php  
- //$connect = mysqli_connect("localhost", "root", "", "farmaciasocial");  
-include("db_connect.php");
- $sql = "DELETE FROM medicos WHERE ID = '".$_POST["id"]."'";  
- if(mysqli_query($connect, $sql))  
- {  
-      echo 'Data Deleted';  
- }  
- ?>
